f277383af8441846211758f2554680ece9ff3c88
mdsami <mdsami.work@gmail.com>
2022-04-05 15:00:11 +0600

    portfolio update

---
+++ resources/views/layouts/frontend/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en"> 
+
+
+<head>
+	<title>MD SAMI &amp; Tech Lead & Tech Enthusiast</title>
+	
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="DevCard Bootstrap 4 Template">
+	<meta name="author" content="Xiaoying Riley at 3rd Wave Media">    
+	<link rel="shortcut icon" href="favicon.ico"> 
+	
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+	
+	<!-- FontAwesome JS -->
+    <script defer src="assets/fontawesome/js/all.js"></script>
+	
+	<!-- Owl Stylesheets -->
+    <link href="{{ asset('assets/plugins/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/plugins/owlcarousel/assets/owl.theme.default.min.css') }}" rel="stylesheet">
+	
+	
+	<!-- Theme CSS -->  
+    <link id="theme-style" href="{{ asset('assets/css/theme-3.css') }}" rel="stylesheet">
+	
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXX"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	
+	  gtag('config', 'UA-XXXXXXX');
+	</script>
+	
+
+
+</head> 
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Frontend\HomeController;
+// use App\Http\Controllers\Frontend\UBIDController;
+// use App\Http\Controllers\Frontend\UBIDController;
+// use App\Http\Controllers\Frontend\UBIDController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('frontend/index');
+// });
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('home', [HomeController::class, 'index'])->name('home');
+Route::get('portfolio', [HomeController::class, 'portfolio'])->name('portfolio');
+Route::get('services', [HomeController::class, 'services'])->name('services');
+Route::get('resume', [HomeController::class, 'resume'])->name('resume');
+Route::get('contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('project', [HomeController::class, 'project'])->name('project');
+Route::get('blog', [HomeController::class, 'blog'])->name('blog');
