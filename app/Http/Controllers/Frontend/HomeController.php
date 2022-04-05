091f18e12beb5c54d97c10edc857fad0557ff624
mdsami <mdsami.work@gmail.com>
2022-04-05 15:05:36 +0600

    cms update

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -11,4 +11,36 @@ class HomeController extends Controller
     {
         return view('frontend.index');
     }
+
+    public function portfolio()
+    {
+        return view('frontend.portfolio');
+    }
+
+    public function services()
+    {
+        return view('frontend.services');
+    }
+
+    public function resume()
+    {
+        return view('frontend.resume');
+    }
+
+    public function contact()
+    {
+        return view('frontend.contact');
+    }
+
+    public function project()
+    {
+        return view('frontend.project');
+    }
+
+    public function blog()
+    {
+        return view('frontend.blog');
+    }
+
+
 }
